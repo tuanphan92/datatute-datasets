abf9269249d96abe569ed196b90d5f03f4ec64ea
Tuan Phan <107517328+tuanphan92@users.noreply.github.com>
2025-08-15 22:00:17 +0700

    Update base_reader.py

--- data-engineering-booster/exercise1/reader/base_reader.py
+++ data-engineering-booster/exercise1/reader/base_reader.py
@@ -9,9 +9,6 @@ class DataReader(ABC):
     In a factory pattern, this acts as the Product interface for operations that all concrete readers must implement
     """
 
-    def __init__(self, config: dict) -> None:
-        self.config = config
-
     @abstractmethod
     def read_data(self) -> pd.DataFrame:
         pass
